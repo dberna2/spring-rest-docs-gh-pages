1828cac476fe45733ae50bf65425ae1e43095810
David Leonardo Bernal <leo.bernal1946@gmail.com>
2021-12-12 16:42:46 +0100

    first commit

---
+++ src/test/java/com/dberna2/springrestdocts/springrestdocs/SpringRestDoctsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dberna2.springrestdocts.springrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDoctsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
